bde76b55e22b38e7b7eb696eee52163854da0c91
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-06-13 18:02:24 +0200

    submodule changes

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 CWD=`pwd`
-git submodule update -i
-cp -a "$CWD/lss" "$CWD/breakpad/src/third_party/" || exit 1
+git submodule update -i --recursive
 cd "$CWD/breakpad";./configure && make -j8 || exit 1
 cd "$CWD/minidump-stackwalk";make || exit 1
-mv "$CWD/minidump-stackwalk/stackwalker" "$CWD/" || exit 1
