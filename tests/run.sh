0ebe7166c3b6f90da4a4e8b68fc1b607c5da7c29
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2019-07-01 05:53:03 +0200

    enable tests again

--- tests/run.sh
+++ tests/run.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 SW="../minidump-stackwalk/stackwalker"
-SYM="symbols"
-DUMPS="39eb2108-ca98-7946-09d36d98-51c56b32 6537E627-678E-44B3-9116-36323C0BA4CC f97fd058-fb43-4098-b770-7fce7a3fb757"
+DUMPS="2f5d2700-d191-9876-430fd7b8-3825119e"
 for i in $DUMPS;do
-  #echo "Testing $i"
-  #rm -f $i.tmp || true
-  $SW --pretty --symbols-url http://fxarena.net/natron/symbols --symbols-cache /tmp $i.dmp #> $i.tmp
-  #DIFF=`diff $i.json $i.tmp`
-  #if [ ! -z "$DIFF" ]; then
-  #  echo "$i: FAILED"
-  #  echo "$DIFF"
-  #  exit 1
-  #else
-  #  echo "$i: PASSED"
-  #fi
-  #rm -f $i.tmp || true
+  echo "Testing $i"
+  rm -f $i.tmp || true
+  $SW --pretty $i.dmp symbols > $i.tmp
+  DIFF=`diff $i.json $i.tmp`
+  if [ ! -z "$DIFF" ]; then
+    echo "$i: FAILED"
+    echo "$DIFF"
+    exit 1
+  else
+    echo "$i: PASSED"
+  fi
+  rm -f $i.tmp || true
 done
-#echo "Tests done"
-#exit 0
